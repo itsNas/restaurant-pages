cd9f5ac4b0c20e8038cf4e7d8813be7f062499da
nasrul-o7 <nasrullah.saloji@gmail.com>
2023-01-24 01:39:24 +0800

    add menu,contact pages. Add style

--- src/js/home.js
+++ src/js/home.js
@@ -2,7 +2,11 @@ function createHome() {
   const home = document.createElement("div")
   home.classList.add("home")
 
-  home.appendChild(createParagraph("Best pizza in your country"))
+  const chefImage = document.createElement("img")
+  chefImage.src = "images/chef.png"
+  chefImage.alt = "Chef"
+
+  home.appendChild(createParagraph("Best Doughnut in your country"))
   home.appendChild(createParagraph("Made with passion since 1908"))
   // home.appendChild(chefImage);
   home.appendChild(createParagraph("Order online or visit us!"))
